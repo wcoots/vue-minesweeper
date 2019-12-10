b2473f6b81180eb39604eb4c7c2595cd216bb109
William Cooter <will@cooters.co.uk>
2019-12-10 01:06:28 +0000

    setting up prettier and applying to all files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,21 @@
 module.exports = {
     root: true,
     env: {
-        node: true
+        node: true,
     },
-    extends: ['plugin:vue/essential', '@vue/airbnb', '@vue/typescript'],
+    extends: [
+        'plugin:vue/essential',
+        '@vue/airbnb',
+        '@vue/typescript',
+        'plugin:prettier/recommended',
+    ],
+    plugins: ['prettier'],
     rules: {
+        'prettier/prettier': 'error',
         'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     },
     parserOptions: {
-        parser: '@typescript-eslint/parser'
-    }
+        parser: '@typescript-eslint/parser',
+    },
 }
--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
     <div id="app">
         <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
-        <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
         <Calculator />
     </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import Calculator from './components/Calculator.vue';
+import { Component, Vue } from 'vue-property-decorator'
+import Calculator from './components/Calculator.vue'
 
 @Component({
-  components: {
-    Calculator,
-  },
+    components: {
+        Calculator,
+    },
 })
 export default class App extends Vue {}
 </script>
