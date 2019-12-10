feb586e711d61633042bddc6fc3e1a0574773d02
William Cooter <will@cooters.co.uk>
2019-12-10 00:24:40 +0000

    linting and created base component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+    <div id="app">
+        <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+        <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
+        <Calculator />
+    </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import Calculator from './components/Calculator.vue';
 
 @Component({
   components: {
-    HelloWorld,
+    Calculator,
   },
 })
 export default class App extends Vue {}
@@ -19,11 +20,11 @@ export default class App extends Vue {}
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
