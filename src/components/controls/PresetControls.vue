f387311388e35e711b5d2b052ad110926ba2de00
William Cooter <will@cooters.co.uk>
2020-01-03 00:22:31 +0000

    did preset controls and started specified
    preset isn't very good, should maybe make the buttons their own components. could also import the dimensions from scripts

--- src/components/controls/PresetControls.vue
+++ src/components/controls/PresetControls.vue
@@ -1,6 +1,20 @@
 <template>
     <div class="controls" oncontextmenu="return false;">
-        <p>PRESET CONTROLS</p>
+        <div class="game_mode" @click="selectGameMode('beginner')">
+            <span><b>Beginner</b></span>
+            <hr />
+            <span>9 x 9, 10 mines</span>
+        </div>
+        <div class="game_mode" @click="selectGameMode('intermediate')">
+            <span><b>Intermediate</b></span>
+            <hr />
+            <span>16 x 16, 40 mines</span>
+        </div>
+        <div class="game_mode" @click="selectGameMode('expert')">
+            <span><b>Expert</b></span>
+            <hr />
+            <span>30 x 16, 99 mines</span>
+        </div>
     </div>
 </template>
 
@@ -10,12 +24,17 @@ import { GameStatus, GameMode } from '@/types'
 
 export default Vue.extend({
     name: 'PresetControls',
+    methods: {
+        selectGameMode(preset_name: GameMode['preset_name']) {
+            this.$store.commit('setupGame', { mode: 'preset', preset_name } as GameMode)
+        },
+    },
 })
 </script>
 
 <style scoped>
 .controls {
-    height: 50px;
+    height: auto;
     width: 100%;
     float: left;
     display: block;
@@ -25,5 +44,20 @@ export default Vue.extend({
     border-right: solid #8c8c8c 2px;
     border-bottom: solid #8c8c8c 2px;
     margin-bottom: 50px;
+}
+.game_mode {
+    position: relative;
+    width: 90%;
+    height: 60px;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    color: #465563;
+    font-size: 14px;
+    text-align: center;
+    margin: 0 auto;
+    margin-top: 7px;
+    margin-bottom: 7px;
+    padding: 5px;
+    border: none;
+    background-color: #e2dfdf;
 }
 </style>
--- src/components/controls/SpecifiedControls.vue
+++ src/components/controls/SpecifiedControls.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="controls" oncontextmenu="return false;">
-        <p>SPECIFIED CONTROLS</p>
+        <input v-model="x" label="x" type="text" spellcheck="false" />
+        <input v-model="y" type="text" spellcheck="false" />
+        <input v-model="mines" type="text" spellcheck="false" />
     </div>
 </template>
 
@@ -10,12 +12,19 @@ import { GameStatus, GameMode } from '@/types'
 
 export default Vue.extend({
     name: 'SpecifiedControls',
+    data() {
+        return {
+            x: 0 as number,
+            y: 0 as number,
+            mines: 0 as number,
+        }
+    },
 })
 </script>
 
 <style scoped>
 .controls {
-    height: 50px;
+    height: auto;
     width: 100%;
     float: left;
     display: block;
@@ -25,5 +34,28 @@ export default Vue.extend({
     border-right: solid #8c8c8c 2px;
     border-bottom: solid #8c8c8c 2px;
     margin-bottom: 50px;
+    padding-top: 10px;
+    padding-bottom: 10px;
+}
+input {
+    width: 60%;
+    height: 20px;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    color: #465563;
+    font-size: 17px;
+    float: left;
+    margin-left: 10px;
+    background-color: #e2dfdf;
+    border: none;
+}
+.submit {
+    width: 20%;
+    height: 22px;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    font-size: 17px;
+    text-align: middle;
+    float: right;
+    margin-right: 10px;
+    border: none;
 }
 </style>
